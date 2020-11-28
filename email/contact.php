9f9483b5cdeb0538ce047a9516e45cee27da07f4
praveen <praveen@praveens-MacBook-Pro.local>
2020-11-28 15:32:02 +0530

    new pages aded

--- email/contact.php
+++ email/contact.php
@@ -1,34 +1,29 @@
 <html>
-   
    <head>
       <title>Sending HTML email using PHP</title>
    </head>
-   
    <body>
+        <?php
+            $name = $_POST['name'];
+            $phonenumber = $_POST['phonenumber'];
+            $message = $_POST['message'];
 
-<?php
-$name = $_POST['name'];
-$phonenumber = $_POST['phonenumber'];
-$message = $_POST['message'];
+            $to = "mailme24praveen@gmail.com";
+            $subject = "Avalanche Motorsports - Contact Us ";
+            $body = "Name: ".$name."\nEmail: ".$phonenumber."\nMessage: ".$message;
+            $header = "From:leads@github.io/ \r\n";
+            $header .= "MIME-Version: 1.0\r\n";
+            $header .= "Content-type: text/html\r\n";
 
-$to = "mailme24praveen@gmail.com";
-$subject = "RIA Emails";
-$body = "Name: ".$name."\nEmail: ".$phonenumber."\nMessage: ".$message;
-$header = "From:leads@github.io/ \r\n";
-$header .= "MIME-Version: 1.0\r\n";
-$header .= "Content-type: text/html\r\n";
-
-//send email
-$retval = mail ($to,$subject,$body,$header);
+            //send email
+            $retval = mail ($to,$subject,$body,$header);
          
-if( $retval == true ) {
-   echo "Message sent successfully...";
-}else {
-   echo "Message could not be sent...";
-}
-?>
-
-
-</body>
+            if( $retval == true ) {
+                echo "Message sent successfully...";
+            } else {
+                echo "Message could not be sent...";
+            }
+        ?>
+    </body>
 </html>
 
