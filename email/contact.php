d552df18969507195345f675099902b4936eeff8
praveen <praveen@praveens-MacBook-Pro.local>
2020-11-28 12:19:46 +0530

    email

---
+++ email/contact.php
@@ -0,0 +1,34 @@
+<html>
+   
+   <head>
+      <title>Sending HTML email using PHP</title>
+   </head>
+   
+   <body>
+
+<?php
+$name = $_POST['name'];
+$phonenumber = $_POST['phonenumber'];
+$message = $_POST['message'];
+
+$to = "mailme24praveen@gmail.com";
+$subject = "RIA Emails";
+$body = "Name: ".$name."\nEmail: ".$phonenumber."\nMessage: ".$message;
+$header = "From:leads@avalancheracing.com \r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/html\r\n";
+
+//send email
+$retval = mail ($to,$subject,$body,$header);
+         
+if( $retval == true ) {
+   echo "Message sent successfully...";
+}else {
+   echo "Message could not be sent...";
+}
+?>
+
+
+</body>
+</html>
+
