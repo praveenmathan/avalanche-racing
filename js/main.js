681783a395c891600393443a3284285d3459655d
praveen <praveen@praveens-MacBook-Pro.local>
2020-11-29 00:29:39 +0530

    enquiry form added

--- js/main.js
+++ js/main.js
@@ -289,4 +289,22 @@
         }
     });
 
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function () {
+                if ($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+
 })(jQuery);
